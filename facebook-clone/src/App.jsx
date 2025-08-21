8c9cc966aefa86d559ac64c4a9870214285ce0c3
AshiwaniKumar-hacker <kumarashiwani532@gmail.com>
2025-08-21 10:38:04 +0530

    Add footer header and sidebar

--- facebook-clone/src/App.jsx
+++ facebook-clone/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Sidebar from './components/Sidebar.jsx'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='app-container'>
+    <Sidebar/>
+    <Header/>
+    <Footer/>
+  </div>
   )
 }
 
