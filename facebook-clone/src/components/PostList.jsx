391e59d28cd00d139014fb9e346f429ada961014
AshiwaniKumar-hacker <kumarashiwani532@gmail.com>
2025-08-30 14:53:19 +0530

    useffect

--- facebook-clone/src/components/PostList.jsx
+++ facebook-clone/src/components/PostList.jsx
@@ -1,20 +1,37 @@
 import { useContext } from "react";
+import { useEffect,useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 const PostList =()=>{
-const {postList,addInitialPosts}=  useContext(PostListData)
+const {postList,addInitialPosts}=  useContext(PostListData);
+const [fetching,setFetching]=useState(false);
 
-const handleGetPostsClick=()=>{
-fetch('https://dummyjson.com/posts')
+
+useEffect(()=>{
+  setFetching(true);
+
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  fetch('https://dummyjson.com/posts',{signal})
 .then((res) => res.json())
-.then((data)=>{addInitialPosts(data.posts)});
+.then((data)=>{addInitialPosts(data.posts)
+  setFetching(false);
+});
+return ()=>{
+  console.log("Cleaning up useEffect.");
+  controller.abort();
 }
+
+},[])
 
   return (
 <div className="post">
-  {postList.length===0 && <WelcomeMessage onGetPostsClick={handleGetPostsClick}/>}
-{postList.map((post)=>(
+  {fetching &&<LoadingSpinner/>}
+  {!fetching && postList.length === 0 && <WelcomeMessage/>}
+{!fetching && postList.map((post)=>(
   <Post key={post.id} post={post}/>)
   )}
 </div>
